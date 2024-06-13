5a4f5c092cd19b84209ee842ebdb70d99b69e3b8
Giacomo Stevanato <giaco.stevanato@gmail.com>
2024-06-13 19:02:23 +0200

    Add TODO

--- src/strategy/improvement/improve.rs
+++ src/strategy/improvement/improve.rs
@@ -19,6 +19,7 @@ pub fn improve<S: StrategyMut>(
     strategy.update_each(graph, |n0, mut n1| {
         // For each successor check if its play profile is better
         for n2 in graph.successors_of(n0) {
+            // TODO: Handle special case where profiles[n0].most_relevant = n0
             if profiles[n1].cmp(&profiles[n2], graph).is_lt() {
                 // If it's better update the strategy
                 n1 = n2;
