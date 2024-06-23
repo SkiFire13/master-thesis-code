9402ef05e8ac157dfbd98aad3919a43b42749c53
Giacomo Stevanato <giaco.stevanato@gmail.com>
2024-06-23 16:50:26 +0200

    Revert last fix

--- mucalc/tests/x.py
+++ mucalc/tests/x.py
@@ -8,9 +8,9 @@ op = sys.argv[1]
 if op == "aut":
     base = sys.argv[2]
     f = f"{base}/{base}"
-    subprocess.run(['./mcrl22lps', f'{f}.mcrl2', f'{f}.lps' '--timings'])
-    subprocess.run(['./lps2lts', f'{f}.lps', f'{f}.lts', '--timings'])
-    subprocess.run(['./ltsconvert', f'{f}.lts', f'{f}.aut', '--timings'])
+    subprocess.run(['mcrl22lps', f'{f}.mcrl2', f'{f}.lps' '--timings'])
+    subprocess.run(['lps2lts', f'{f}.lps', f'{f}.lts', '--timings'])
+    subprocess.run(['ltsconvert', f'{f}.lts', f'{f}.aut', '--timings'])
     os.remove(f"{f}.lps")
     os.remove(f"{f}.lts")
 elif op == "solve":
@@ -18,9 +18,9 @@ elif op == "solve":
     formula = sys.argv[3]
     f = f"{base}/{base}"
     g = f"{base}/{formula}"
-    subprocess.run(['./mcrl22lps', f'{f}.mcrl2', f'{f}.lps', '--timings'])
-    subprocess.run(['./lps2pbes', f'--formula={g}.mcf', f'{f}.lps', f'{g}.pbes', '--timings'])
-    subprocess.run(['./pbes2bool', '-rjitty', f'{g}.pbes', '--timings'])
+    subprocess.run(['mcrl22lps', f'{f}.mcrl2', f'{f}.lps', '--timings'])
+    subprocess.run(['lps2pbes', f'--formula={g}.mcf', f'{f}.lps', f'{g}.pbes', '--timings'])
+    subprocess.run(['pbes2bool', '-rjitty', f'{g}.pbes', '--timings'])
     os.remove(f"{f}.lps")
     os.remove(f"{g}.pbes")
 else:
