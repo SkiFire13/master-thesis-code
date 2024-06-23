51d62d791eb96ace12dd6b85032d53c13b5a789e
Giacomo Stevanato <giaco.stevanato@gmail.com>
2024-06-23 16:03:08 +0200

    Require less explicit paths in x.py

--- mucalc/tests/x.py
+++ mucalc/tests/x.py
@@ -1,17 +1,23 @@
 import sys, subprocess, os
 
+dir = os.path.dirname(os.path.realpath(__file__))
+os.chdir(dir)
+
 op = sys.argv[1]
 
 if op == "aut":
-    f = sys.argv[2]
+    base = sys.argv[2]
+    f = f"{base}/{base}"
     subprocess.call(f'mcrl22lps "{f}.mcrl2" "{f}.lps" --timings')
     subprocess.call(f'lps2lts "{f}.lps" "{f}.lts" --timings')
     subprocess.call(f'ltsconvert "{f}.lts" "{f}.aut" --timings')
     os.remove(f"{f}.lps")
     os.remove(f"{f}.lts")
 elif op == "solve":
-    f = sys.argv[2]
-    g = sys.argv[3]
+    base = sys.argv[2]
+    formula = sys.argv[3]
+    f = f"{base}/{base}"
+    g = f"{base}/{formula}"
     subprocess.call(f'mcrl22lps "{f}.mcrl2" "{f}.lps" --timings')
     subprocess.call(f'lps2pbes --formula="{g}.mcf" "{f}.lps" "{g}.pbes" --timings')
     subprocess.call(f'pbes2bool -rjitty "{g}.pbes" --timings')
