f6acc91cb5e332e6e38bad8afdff9a8a6868fdf4
Giacomo Stevanato <giaco.stevanato@gmail.com>
2024-06-25 15:53:45 +0200

    Extend aut parser to be more flexible

--- mucalc/src/parser.rs
+++ mucalc/src/parser.rs
@@ -12,14 +12,15 @@ pub fn parse_alt(source: &str) -> Result<Lts> {
     let mut lines = source.lines();
 
     let header = lines.next().context("File is empty")?;
-    let header = header.strip_prefix("des (").context("Expected 'des ('")?;
+    let header = header.strip_prefix("des").context("Expected 'des'")?;
+    let header = header.trim().strip_prefix("(").context("Expected '('")?;
     let (first_state, header) = header.split_once(',').context("Expected first state")?;
     let (trans_count, header) = header.split_once(',').context("Expected trans count")?;
     let state_count = header.strip_suffix(")").context("Expected state count")?;
 
-    let first_state = first_state.parse().context("Expected first state to be a number")?;
-    let trans_count = trans_count.parse().context("Expected trans count to be a number")?;
-    let state_count = state_count.parse().context("Expected state count to be a number")?;
+    let first_state = first_state.trim().parse().context("Expected first state to be a number")?;
+    let trans_count = trans_count.trim().parse().context("Expected trans count to be a number")?;
+    let state_count = state_count.trim().parse().context("Expected state count to be a number")?;
 
     if first_state >= state_count {
         bail!("First state {first_state} doesn't exist")
@@ -31,12 +32,19 @@ pub fn parse_alt(source: &str) -> Result<Lts> {
 
     for line in lines {
         let line = line.strip_prefix('(').context("Expected '('")?;
-        let (start_state, line) = line.split_once(",\"").context("Expected start state")?;
-        let (label, line) = line.split_once("\",").context("Expected label")?;
+        let (start_state, line) = line.split_once(',').context("Expected start state")?;
+        let (label, line) = match line.trim_start().strip_prefix('"') {
+            Some(line) => {
+                let (label, line) = line.split_once('"').context("Expected label '\"'")?;
+                let line = line.trim_start().strip_prefix(',').context("Expected label ','")?;
+                (label, line)
+            }
+            None => line.split_once(',').context("Expected label")?,
+        };
         let end_state = line.strip_suffix(')').context("Expected end state")?;
 
-        let start_state = start_state.parse().context("Start state is not a number")?;
-        let end_state = end_state.parse().context("End state is not a number")?;
+        let start_state = start_state.trim().parse().context("Start state is not a number")?;
+        let end_state = end_state.trim().parse().context("End state is not a number")?;
 
         if start_state >= state_count {
             bail!("Start state {start_state} doesn't exist")
@@ -45,7 +53,7 @@ pub fn parse_alt(source: &str) -> Result<Lts> {
             bail!("End state {end_state} doesn't exist")
         }
 
-        transitions[StateId(start_state)].push((label.to_string(), StateId(end_state)));
+        transitions[StateId(start_state)].push((label.trim().to_string(), StateId(end_state)));
         transitions_count += 1;
     }
 
